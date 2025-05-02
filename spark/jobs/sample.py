c07c2a844e7e95c085917ab4742ecec7daca57a9
tyagiabhinav999 <tyagiabhinav999@gmail.com>
2025-05-02 20:56:47 +0530

    update master to execute spark-submit inside spark-master container

--- spark/jobs/sample.py
+++ spark/jobs/sample.py
@@ -5,7 +5,7 @@ def main():
     # Initialize SparkSession
     spark = SparkSession.builder \
         .appName("HelloWorld") \
-        .master("spark://localhost:7077") \
+        .master("spark://spark-master:7077") \
         .getOrCreate()
 
     # Create an RDD containing numbers from 1 to 1000
